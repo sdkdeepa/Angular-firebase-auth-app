126ea9e357341c75f095f40b5a7b7b05ef90d504
sdkdeepa <deepa94086@gmail.com>
2022-04-21 19:42:53 -0700

    GDSC demo app

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'gdsc-firebase-angular-auth',
+    appId: '',
+    storageBucket: '',
+    apiKey: '',
+    authDomain: '',
+    messagingSenderId: '',
+  },
+  production: true
+};
